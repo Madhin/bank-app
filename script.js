7c5bc186cbc4fdfd71fbe41955b5df77df649fb4
Madhin <madhinhussain@gmail.com>
2021-10-12 16:46:28 +0100

    functionality updated

--- script.js
+++ script.js
@@ -173,8 +173,39 @@ else {
   alert('Invalid Transfer')
 }
 
+});
+
+btnLoan.addEventListener('click', () => {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value)
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount)
+  } 
+  else {alert('Loan amount greater than 10% of transactions' )}
 })
 
+btnClose.addEventListener('click', (e) => {
+e.preventDefault();
+
+if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+  const index = accounts.findIndex(acc => acc.username === currentAccount.username)
+  
+  console.log(index);
+  accounts.splice(index, 1)
+
+  containerApp.style.opacity = 100;
+} else {
+  alert('Invalid Account')
+}
+
+inputCloseUsername.value = inputClosePin.value = '';
+})
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
